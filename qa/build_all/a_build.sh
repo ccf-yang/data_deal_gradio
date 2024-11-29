b5c210ed594a0e60057cec47ee52ea5e23a56d2b
tangmu <tangmu@pplabs.org>
2024-11-29 14:17:22 +0800

    add dockerfile and docker-compose v3

---
+++ qa/build_all/a_build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir -p ../data/mysql ../data/redis
+
+docker-compose up -d --build
+
+# --build 参数会让每次启动都会重新构建
+
+# 停止: docker-compose stop
+
+# 启动: docker-compose start
+
+# 重启: docker-compose restart
+
+# 删除: docker-compose down
+
+# 查看log: docker-compose logs qa_api
--- qa/build_script/a_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p ../data/mysql ../data/redis
-
-docker-compose up -d
-
-# 查看log: docker-compose logs qa_api
